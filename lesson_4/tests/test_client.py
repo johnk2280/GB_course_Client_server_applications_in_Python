ddf503da18c8dbeb400e677d6572da25f6393326
johnk2280 <johnk2280@gmail.com>
2021-11-29 23:29:43 +0500

    [add] tests

--- lesson_4/tests/test_client.py
+++ lesson_4/tests/test_client.py
@@ -1,21 +1,60 @@
+import json
 import unittest
 
 from lesson_4.client.client import Client, parse_command_line
-
-
-class TestParseCommandLine(unittest.TestCase):
-
-    def test_parse_command_line(self):
-        pass
+from lesson_4.server.server import MessageServer
 
 
 class TestClient(unittest.TestCase):
 
     def test_create_message(self):
-        pass
+        server = MessageServer('127.0.0.1', 8888)
+        client = Client('127.0.0.1', 8888)
+        data = {
+            "action": "presence",
+            "time": '',
+            "type": "status",
+            "user": {
+                "account_name": "C0deMaver1ck",
+                "status": "Yep, I am here!",
+            }
+        }
+
+        self.assertEqual(client.create_message(), json.dumps(data).encode('utf-8'))
+        client.sock.close()
+        server.sock.close()
 
     def test_send_request(self):
-        pass
+        server = MessageServer('127.0.0.1', 8888)
+        client = Client('127.0.0.1', 8888)
+        data = {
+            "action": "presence",
+            "time": '',
+            "type": "status",
+            "user": {
+                "account_name": "C0deMaver1ck",
+                "status": "Yep, I am here!",
+            }
+        }
 
-    def get_response(self):
-        pass
+        self.assertEqual(client.send_request(json.dumps(data).encode('utf-8')), None)
+        client.sock.close()
+        server.sock.close()
+
+    def test_get_response(self):
+        server = MessageServer('127.0.0.1', 8888)
+        client = Client('127.0.0.1', 8888)
+        data = {
+            "action": "presence",
+            "time": '',
+            "type": "status",
+            "user": {
+                "account_name": "C0deMaver1ck",
+                "status": "Yep, I am here!",
+            }
+        }
+
+        self.assertEqual(client.get_response(json.dumps(data).encode('utf-8')), data)
+        client.sock.close()
+        server.sock.close()
+
